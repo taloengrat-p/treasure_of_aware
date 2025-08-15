1b2fff033bdb075888cc576e55e425e83824da2c
taloengrat.p <taloengrat.p@aware.co.th>
2025-08-15 11:14:57 +0700

    feature : change dialog widget location

--- lib/widgets/dialogs/treasure_success_alert/treasure_success_alert.dart
+++ lib/widgets/dialogs/treasure_success_alert/treasure_success_alert.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:treasure_of_aware/dialogs/treasure_success_alert/widget/treasure_content.dart';
+import 'package:treasure_of_aware/widgets/dialogs/treasure_success_alert/widget/treasure_content.dart';
 import 'package:treasure_of_aware/models/treasure.dart';
 import 'package:treasure_of_aware/models/treasure_item.dart';
 
 class TreasureSuccessAlert {
-  static Future<bool?> show(
-    BuildContext context, {
-    required Treasure treasure,
-    required TreasureItem treasureItem,
-  }) {
+  static Future<bool?> show(BuildContext context, {required Treasure treasure, required TreasureItem treasureItem}) {
     return showDialog(
       context: context,
       builder: (context) {
         return Dialog(
           backgroundColor: Colors.transparent,
-          child: TreasureContent(
-            treasure: treasure,
-            treasureItem: treasureItem,
-          ),
+          child: TreasureContent(treasure: treasure, treasureItem: treasureItem),
         );
       },
     );
